18ce1f55e18dbf57cf4ffe2b69ce9e55738bd608
Matt DeKrey <matt.dekrey@principlestudios.com>
2024-02-07 11:37:48 -0600

    Add eslint configs

---
+++ .eslintrc.cjs
@@ -0,0 +1,44 @@
+/** @type {import('eslint').Linter.Config} */
+module.exports = {
+	root: true,
+	plugins: ['@typescript-eslint'],
+	extends: [
+		// The order of these matter:
+		// eslint baseline
+		'eslint:recommended',
+		// disables eslint rules in favor of using prettier separately
+		'prettier',
+		'plugin:react-hooks/recommended',
+		'plugin:storybook/recommended',
+	],
+	rules: {
+		// https://typescript-eslint.io/linting/troubleshooting/#i-get-errors-from-the-no-undef-rule-about-global-variables-not-being-defined-even-though-there-are-no-typescript-errors
+		'no-undef': 'off',
+	},
+	ignorePatterns: ['**/generated/**/*', '*/dev-dist/**/*'],
+	parserOptions: {
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+	},
+	overrides: [
+		{
+			files: ['**/*.{ts,tsx}'],
+			extends: [
+				// Recommended typescript changes, which removes some "no-undef" checks that TS handles
+				'plugin:@typescript-eslint/eslint-recommended',
+				'plugin:@typescript-eslint/recommended-requiring-type-checking',
+				'plugin:@typescript-eslint/recommended',
+			],
+			rules: {
+				'@typescript-eslint/consistent-type-imports': [
+					'error',
+					{
+						disallowTypeAnnotations: false,
+					},
+				],
+				// no-unsafe-assignment complains when passing components as variables
+				'@typescript-eslint/no-unsafe-assignment': [0],
+			},
+		},
+	],
+};

69413c8a08ffe777bdc55831cb2b8995e7241ca2
Matt DeKrey <matt.dekrey@principlestudios.com>
2024-02-13 08:20:54 -0600

    Ensure PowerShell runs cross-platform

--- Server/ShellUtililties/PowerShellFactory.cs
+++ Server/ShellUtililties/PowerShellFactory.cs
@@ -1,50 +1,37 @@
 ﻿using System.Management.Automation.Runspaces;
 using System.Management.Automation;
 
-namespace PrincipleStudios.ScaledGitApp.ShellUtililties
+namespace PrincipleStudios.ScaledGitApp.ShellUtililties;
+
+public class PowerShellFactory
 {
-	public class PowerShellFactory
+	private readonly InitialSessionState initialSessionState;
+
+	public PowerShellFactory()
 	{
-		private static readonly string[] continuePreferences =
-		{
-			"DebugPreference",
-			"ProgressPreference",
-			"VerbosePreference",
-			"WarningPreference",
-			"InformationPreference",
-		};
-		private readonly InitialSessionState initialSessionState;
-
-		public PowerShellFactory()
-		{
-			initialSessionState = InitialSessionState.CreateDefault();
-			initialSessionState.ExecutionPolicy = Microsoft.PowerShell.ExecutionPolicy.Unrestricted;
-			initialSessionState.Variables.Add(new SessionStateVariableEntry("ErrorActionPreference", ActionPreference.Stop, "Override error action preference"));
-			foreach (var pref in continuePreferences)
-				initialSessionState.Variables.Add(new SessionStateVariableEntry(pref, ActionPreference.Continue, "Override stream preference"));
-		}
-
-		public Runspace CreateRunspace(Action<InitialSessionState>? configure = null)
-		{
-			var iss = initialSessionState;
-
-			if (configure != null)
-			{
-				iss = initialSessionState.Clone();
-				configure.Invoke(iss);
-			}
-
-			var result = RunspaceFactory.CreateRunspace(iss);
-			result.Open();
-			return result;
-		}
-
-		public IPowerShell Create(Runspace? runspace = null)
-		{
-			return new PowerShellWrapperImplementation(runspace != null
-				? PowerShell.Create(runspace)
-				: PowerShell.Create(initialSessionState));
-		}
-
+		initialSessionState = PowerShellSessionStateUtility.BuildStandardSessionState();
 	}
+
+	public Runspace CreateRunspace(Action<InitialSessionState>? configure = null)
+	{
+		var iss = initialSessionState;
+
+		if (configure != null)
+		{
+			iss = initialSessionState.Clone();
+			configure.Invoke(iss);
+		}
+
+		var result = RunspaceFactory.CreateRunspace(iss);
+		result.Open();
+		return result;
+	}
+
+	public IPowerShell Create(Runspace? runspace = null)
+	{
+		return new PowerShellWrapperImplementation(runspace != null
+			? PowerShell.Create(runspace)
+			: PowerShell.Create(initialSessionState));
+	}
+
 }

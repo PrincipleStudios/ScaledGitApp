9506a69d2d4a1b564197b9f7eee5e1ba7fa4cd63
Matt DeKrey <matt.dekrey@principlestudios.com>
2024-02-16 14:42:45 -0600

    Runspaces are not thread-safe; refactoring
    Learned this by running multiple `fetch` requests

--- Server.Tests/Git/GitToolsPowerShellInvokerShould.cs
+++ Server.Tests/Git/GitToolsPowerShellInvokerShould.cs
@@ -13,8 +13,8 @@ public partial class GitToolsPowerShellInvokerShould
 	{
 		fixture.MockPowerShellFactory.Setup(ps => ps.Create(null)).Returns((IPowerShell)null!);
 
-		// By mocking the factory directly, we test the typical DI constructor, which should not call immediately
-		using var target = fixture.CreateTarget(mockFactoryDirectly: false);
+		// By not mocking the factory, we test the typical DI constructor with working directory setup
+		var target = fixture.CreateTarget(mockFactoryDirectly: false);
 
 		await Task.Yield();
 
@@ -28,29 +28,29 @@ public partial class GitToolsPowerShellInvokerShould
 		var baseWorkingDirectory = Directory.GetCurrentDirectory();
 		var expectedWorkingDirectory = Path.TrimEndingDirectorySeparator(Path.GetTempPath());
 		var mockFindGitRoot = new Mock<IPowerShell>();
-		using var runspace = System.Management.Automation.Runspaces.RunspaceFactory.CreateRunspace();
 		var mockFinal = new Mock<IPowerShell>();
-		runspace.Open();
 
-		fixture.MockPowerShellFactory.Setup(ps => ps.Create(null)).Returns(mockFindGitRoot.Object);
+		var creatPowershell = fixture.MockPowerShellFactory.VerifiableSequence(
+			ps => ps.Create(null),
+			s => s.Returns(mockFindGitRoot.Object).Returns(mockFinal.Object)
+		);
 		mockFindGitRoot.Setup(ps => ps.SetCurrentWorkingDirectory(baseWorkingDirectory));
 		mockFindGitRoot.Setup(ps => ps.InvokeCliAsync("git", "rev-parse", "--show-toplevel"))
 			.ReturnsAsync(PowerShellInvocationResultStubs.WithResults(expectedWorkingDirectory));
-		fixture.MockPowerShellFactory.Setup(ps => ps.CreateRunspace(null)).Returns(runspace);
-		var createdWithRunspace = fixture.MockPowerShellFactory.Verifiable(ps => ps.Create(runspace), s => s.Returns(mockFinal.Object));
+		var workingDirectorySet = mockFinal.Verifiable(ps => ps.SetCurrentWorkingDirectory(expectedWorkingDirectory));
 
 		var mockCommand = new Mock<IGitToolsCommand<Task<string>>>();
 		var verifiableCommand = mockCommand.Verifiable(cmd => cmd.RunCommand(It.IsAny<IGitToolsPowerShell>()), s => s.ReturnsAsync(expectedResult));
 
-		// By mocking the factory directly, we test the typical DI constructor with working directory setup
-		using var target = fixture.CreateTarget(mockFactoryDirectly: false);
+		// By not mocking the factory, we test the typical DI constructor with working directory setup
+		var target = fixture.CreateTarget(mockFactoryDirectly: false);
 
 		var result = await target.RunCommand(mockCommand.Object);
 
-		Assert.Equal(expectedWorkingDirectory, Path.TrimEndingDirectorySeparator(runspace.SessionStateProxy.Path.CurrentLocation.Path));
 		Assert.Equal(expectedResult, result);
-		createdWithRunspace.Verify(Times.Once);
+		creatPowershell.Verify(Times.Exactly(2));
 		verifiableCommand.Verify(Times.Once);
+		workingDirectorySet.Verify(Times.Once);
 	}
 
 	[Fact]
@@ -64,7 +64,7 @@ public partial class GitToolsPowerShellInvokerShould
 		var verifiableCommand = mockCommand.Verifiable(cmd => cmd.RunCommand(It.IsAny<IGitToolsPowerShell>()), s => s.ReturnsAsync(expectedResult));
 
 		// By mocking the factory directly, we test the typical DI constructor with working directory setup
-		using var target = fixture.CreateTarget();
+		var target = fixture.CreateTarget();
 
 		var result = await target.RunCommand(mockCommand.Object);
 
--- Server/ShellUtilities/PowerShellExtensions.cs
+++ Server/ShellUtilities/PowerShellExtensions.cs
@@ -1,15 +1,12 @@
 ﻿using PrincipleStudios.ScaledGitApp.Git;
 using System.Management.Automation;
-using System.Management.Automation.Runspaces;
 
 namespace PrincipleStudios.ScaledGitApp.ShellUtilities;
 
 public static class PowerShellExtensions
 {
 	public static void SetCurrentWorkingDirectory(this PowerShell shell, string workingDirectory) =>
-		shell.Runspace.SetCurrentWorkingDirectory(workingDirectory);
-	public static void SetCurrentWorkingDirectory(this Runspace runspace, string workingDirectory) =>
-		runspace.SessionStateProxy.Path.SetLocation(workingDirectory);
+		shell.Runspace.SessionStateProxy.Path.SetLocation(workingDirectory);
 
 	public static Task<PowerShellInvocationResult> InvokeCliAsync(this IPowerShell pwsh, string command, IEnumerable<string> arguments) =>
 		pwsh.InvokeCliAsync(command, arguments.ToArray());

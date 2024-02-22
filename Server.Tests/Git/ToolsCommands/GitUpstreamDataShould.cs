11b15fb12af2f59017038a3120d6ebd7374c1c7f
Matt DeKrey <matt.dekrey@principlestudios.com>
2024-02-22 08:31:27 -0600

    Refactor tests

--- Server.Tests/Git/ToolsCommands/GitUpstreamDataShould.cs
+++ Server.Tests/Git/ToolsCommands/GitUpstreamDataShould.cs
@@ -26,49 +26,61 @@ public class GitUpstreamDataShould
 	}
 
 	[Fact]
-	public async Task Handles_a_single_upstream_configuration()
+	public async Task Handles_no_upstream_branch_configuration()
 	{
-		var verifyGitRemote = SetupGitUpstreamDataBranches(fixture.MockPowerShell, new()
-		{
-			{ "feature/PS-123", ["main"] },
-		});
+		Dictionary<string, string[]> branchConfigurations = new();
+		var verifyGitRemote = SetupGitUpstreamDataBranches(fixture.MockPowerShell, branchConfigurations);
 		var target = new GitUpstreamData();
 
 		var branches = await target.RunCommand(fixture.Create());
 
-		var (branchName, config) = Assert.Single(branches);
-		Assert.Equal("feature/PS-123", branchName);
-		Assert.Collection(config.UpstreamBranchNames,
-			actual => Assert.Equal("main", actual)
-		);
+		AssertAllBranchConfigurations(branchConfigurations, branches);
+		verifyGitRemote.Verify(Times.Once);
+	}
+
+	[Fact]
+	public async Task Handles_a_single_upstream_configuration()
+	{
+		Dictionary<string, string[]> branchConfigurations = new()
+		{
+			{ "feature/PS-123", ["main"] },
+		};
+		var verifyGitRemote = SetupGitUpstreamDataBranches(fixture.MockPowerShell, branchConfigurations);
+		var target = new GitUpstreamData();
+
+		var branches = await target.RunCommand(fixture.Create());
+
+		AssertAllBranchConfigurations(branchConfigurations, branches);
 		verifyGitRemote.Verify(Times.Once);
 	}
 
 	[Fact]
 	public async Task Handles_multiple_upstream_configurations()
 	{
-		var verifyGitRemote = SetupGitUpstreamDataBranches(fixture.MockPowerShell, new()
+		Dictionary<string, string[]> branchConfigurations = new()
 		{
 			{ "feature/PS-123", ["main"] },
 			{ "feature/PS-124", ["feature/PS-123", "infra/upgrade"] },
 			{ "infra/upgrade", ["main"] },
-		});
+		};
+		var verifyGitRemote = SetupGitUpstreamDataBranches(fixture.MockPowerShell, branchConfigurations);
 		var target = new GitUpstreamData();
 
-		var branches = await target.RunCommand(fixture.Create());
+		var actualConfigurations = await target.RunCommand(fixture.Create());
 
-		var mainOnlyUpstream = Assert.Contains("feature/PS-123", branches);
-		Assert.Collection(mainOnlyUpstream.UpstreamBranchNames,
-			actual => Assert.Equal("main", actual)
-		);
-		var mainOnlyUpstream2 = Assert.Contains("infra/upgrade", branches);
-		Assert.Collection(mainOnlyUpstream2.UpstreamBranchNames,
-			actual => Assert.Equal("main", actual)
-		);
-		var multipleUpstream = Assert.Contains("feature/PS-124", branches);
-		Assert.Contains("feature/PS-123", multipleUpstream.UpstreamBranchNames);
-		Assert.Contains("infra/upgrade", multipleUpstream.UpstreamBranchNames);
+		AssertAllBranchConfigurations(branchConfigurations, actualConfigurations);
 		verifyGitRemote.Verify(Times.Once);
+	}
+
+	private static void AssertAllBranchConfigurations(IReadOnlyDictionary<string, string[]> expectedResult, IReadOnlyDictionary<string, UpstreamBranchConfiguration> actualResult)
+	{
+		Assert.All(actualResult.Keys, (branchName) => { Assert.Contains(branchName, expectedResult); });
+		foreach (var (expectedBranchName, expectedConfiguration) in expectedResult)
+		{
+			var actualConfiguration = Assert.Contains(expectedBranchName, actualResult);
+			foreach (var branch in expectedConfiguration)
+				Assert.Contains(branch, actualConfiguration.UpstreamBranchNames);
+		}
 	}
 
 	internal static VerifiableMock<IPowerShell, Task<PowerShellInvocationResult>> SetupGitUpstreamDataFailure(Mock<IPowerShell> target)
@@ -81,9 +93,9 @@ public class GitUpstreamDataShould
 		);
 	}
 
-	internal static VerifiableMock<IPowerShell, Task<PowerShellInvocationResult>> SetupGitUpstreamDataBranches(Mock<IPowerShell> target, Dictionary<string, string[]> branchConfigurations)
+	internal static VerifiableMock<IPowerShell, Task<PowerShellInvocationResult>> SetupGitUpstreamDataBranches(Mock<IPowerShell> target, Dictionary<string, string[]> expectedResult)
 	{
-		var entries = branchConfigurations.Select(kvp => (Path: kvp.Key, Hash: string.Join('\n', kvp.Value).GetHashCode().ToString("X40"), Branches: kvp.Value));
+		var entries = expectedResult.Select(kvp => (Path: kvp.Key, Hash: string.Join('\n', kvp.Value).GetHashCode().ToString("X40"), Branches: kvp.Value));
 
 		target.Setup(ps => ps.InvokeCliAsync("git", It.IsAny<PSDataCollection<string>>(), It.Is<IEnumerable<string>>(s => s.First() == "cat-file" && s.ElementAt(1) == "--batch=\t%(objectname)")))
 			.ReturnsAsync(PowerShellInvocationResultStubs.WithResults(

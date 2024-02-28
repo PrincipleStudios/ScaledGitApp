affcd3c5c61437ff4158be19a1114e5dcb91ac5b
Matt DeKrey <matt.dekrey@principlestudios.com>
2024-02-28 15:46:49 -0600

    Move color configuration out of controller

---
+++ Server/BranchingStrategy/ColorConfiguration.cs
@@ -0,0 +1,13 @@
+﻿namespace PrincipleStudios.ScaledGitApp.BranchingStrategy;
+
+public class ColorConfiguration : IColorConfiguration
+{
+	public string DetermineColor(string branchName)
+	{
+		// TODO: move to somewhere with configuration
+		if (branchName.StartsWith("rc/")) return "rgb(111, 37, 111)";
+		if (branchName.StartsWith("line/") || branchName == "main" || branchName == "master") return "rgb(111, 206, 31)";
+		if (branchName.StartsWith("integ/") || branchName.StartsWith("integrate/") || branchName.StartsWith("integration/")) return "rgb(98, 98, 98)";
+		return "rgb(55, 127, 192)";
+	}
+}
---
+++ Server/BranchingStrategy/ServiceRegistration.cs
@@ -0,0 +1,11 @@
+﻿using PrincipleStudios.ScaledGitApp.ShellUtilities;
+
+namespace PrincipleStudios.ScaledGitApp.BranchingStrategy;
+
+public static class ServiceRegistration
+{
+	internal static void RegisterBranchingStrategy(this IServiceCollection services)
+	{
+		services.AddSingleton<IColorConfiguration, ColorConfiguration>();
+	}
+}

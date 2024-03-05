f8f3dbe0bab6997773d41f56172b798b6a0c6238
Matt DeKrey <matt.dekrey@principlestudios.com>
2024-03-05 17:04:32 -0600

    Implement GetCommitCount tests

--- Server.Tests/Git/ToolsCommands/GetCommitCountShould.cs
+++ Server.Tests/Git/ToolsCommands/GetCommitCountShould.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Moq;
+using PrincipleStudios.ScaledGitApp.ShellUtilities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,21 +10,69 @@ namespace PrincipleStudios.ScaledGitApp.Git.ToolsCommands;
 
 public class GetCommitCountShould
 {
+	private readonly PowerShellFixture fixture = new PowerShellFixture();
+
 	[Fact]
-	public void Get_commit_count_of_a_branch()
+	public async Task Get_commit_count_of_a_branch()
 	{
-		throw new NotImplementedException();
+		var included = new string[] { "main" };
+		var verifyGitClone = SetupGetCommitCount(fixture.MockPowerShell, included, [], 15);
+		var target = new GetCommitCount(included, []);
+
+		await target.RunCommand(fixture.Create());
+
+		verifyGitClone.Verify(Times.Once);
 	}
 
 	[Fact]
-	public void Get_commit_count_of_multiple_branches()
+	public async Task Get_commit_count_of_multiple_branches()
 	{
-		throw new NotImplementedException();
+		var included = new string[] { "main", "feature/PS-123" };
+		var verifyGitClone = SetupGetCommitCount(fixture.MockPowerShell, included, [], 15);
+		var target = new GetCommitCount(included, []);
+
+		await target.RunCommand(fixture.Create());
+
+		verifyGitClone.Verify(Times.Once);
 	}
 
 	[Fact]
-	public void Get_commit_count_excluding_branches()
+	public async Task Get_commit_count_excluding_branches()
 	{
-		throw new NotImplementedException();
+		var included = new string[] { "feature/PS-123" };
+		var excluded = new string[] { "main" };
+		var verifyGitClone = SetupGetCommitCount(fixture.MockPowerShell, included, excluded, 1);
+		var target = new GetCommitCount(included, excluded);
+
+		await target.RunCommand(fixture.Create());
+
+		verifyGitClone.Verify(Times.Once);
+	}
+
+	internal static VerifiableMock<IPowerShellCommandContext, Task<PowerShellInvocationResult>> SetupGetCommitCount(Mock<IPowerShellCommandContext> target, string[] included, string[] excluded, int count)
+	{
+		return target.Verifiable(
+			ps => ps.PowerShellInvoker.InvokeCliAsync("git", It.Is<string[]>(args => VerifyCliArgs(args, included, excluded))),
+			s => s.ReturnsAsync(PowerShellInvocationResultStubs.WithResults(count.ToString()))
+		);
+	}
+
+	static readonly string[] defaultSwitches = ["--count", "--no-merges"];
+	static bool VerifyCliArgs(string[] args, string[] included, string[] excluded)
+	{
+		if (args[0] != "rev-list") return false;
+		var argset = new HashSet<string>(args.Skip(1));
+		foreach (var s in defaultSwitches)
+			if (!argset.Remove(s))
+				return false;
+		foreach (var s in included)
+			if (!argset.Remove(s))
+				return false;
+		foreach (var s in excluded)
+			if (!argset.Remove('^' + s))
+				return false;
+
+		if (argset.Count > 0) return false;
+		return true;
 	}
 }
--- Server/Git/ToolsCommands/GetCommitCount.cs
+++ Server/Git/ToolsCommands/GetCommitCount.cs
@@ -2,11 +2,11 @@
 
 namespace PrincipleStudios.ScaledGitApp.Git.ToolsCommands;
 
-public record GetCommitCount(IEnumerable<string> Included, IEnumerable<string> Excluded) : IGitToolsCommand<Task<int?>>
+public record GetCommitCount(IEnumerable<string> Included, IEnumerable<string> Excluded) : IPowerShellCommand<Task<int?>>
 {
 	private static readonly IReadOnlyList<string> countArgs = ["rev-list", "--count", "--no-merges"];
 
-	public async Task<int?> RunCommand(IGitToolsCommandContext pwsh)
+	public async Task<int?> RunCommand(IPowerShellCommandContext pwsh)
 	{
 		var cliResults = await pwsh.InvokeCliAsync("git", countArgs.Concat(Included).Concat(Excluded.Select(b => $"^{b}")).ToArray());
 		if (cliResults.HadErrors) return null;

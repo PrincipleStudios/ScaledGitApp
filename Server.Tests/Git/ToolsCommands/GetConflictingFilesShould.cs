26612a262c8fb37d3bed720b2d1900560788ee5b
Matt DeKrey <matt.dekrey@principlestudios.com>
2024-03-05 17:04:44 -0600

    Switch stubbed tests to `async Task`

--- Server.Tests/Git/ToolsCommands/GetConflictingFilesShould.cs
+++ Server.Tests/Git/ToolsCommands/GetConflictingFilesShould.cs
@@ -3,13 +3,13 @@
 public class GetConflictingFilesShould
 {
 	[Fact]
-	public void Return_new_hash_if_no_conflicts()
+	public Task Return_new_hash_if_no_conflicts()
 	{
 		throw new NotImplementedException();
 	}
 
 	[Fact]
-	public void Return_hash_with_list_of_files_if_conflicts_exist()
+	public Task Return_hash_with_list_of_files_if_conflicts_exist()
 	{
 		throw new NotImplementedException();
 	}
--- Server.Tests/Git/ToolsCommands/GitBranchUpstreamDetailsShould.cs
+++ Server.Tests/Git/ToolsCommands/GitBranchUpstreamDetailsShould.cs
@@ -3,31 +3,31 @@
 public class GitBranchUpstreamDetailsShould
 {
 	[Fact]
-	public void Indicate_the_number_of_commits_the_current_branch_has_beyond_upstreams()
+	public Task Indicate_the_number_of_commits_the_current_branch_has_beyond_upstreams()
 	{
 		throw new NotImplementedException();
 	}
 
 	[Fact]
-	public void Report_number_of_commits_missing_from_upstreams()
+	public Task Report_number_of_commits_missing_from_upstreams()
 	{
 		throw new NotImplementedException();
 	}
 
 	[Fact]
-	public void Handle_when_specified_branch_is_missing()
+	public Task Handle_when_specified_branch_is_missing()
 	{
 		throw new NotImplementedException();
 	}
 
 	[Fact]
-	public void Handle_when_an_upstream_is_missing()
+	public Task Handle_when_an_upstream_is_missing()
 	{
 		throw new NotImplementedException();
 	}
 
 	[Fact]
-	public void Reveal_conflicts_with_an_upstream()
+	public Task Reveal_conflicts_with_an_upstream()
 	{
 		throw new NotImplementedException();
 	}

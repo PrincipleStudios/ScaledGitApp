f38cbfafc468eca90413dd02caab726f5afd918b
Matt DeKrey <matt.dekrey@principlestudios.com>
2024-03-05 08:16:59 -0600

    Remove redundant methods

--- Server/ShellUtilities/IPowerShell.cs
+++ Server/ShellUtilities/IPowerShell.cs
@@ -6,14 +6,11 @@ public interface IPowerShell : IPowerShellInvoker, IDisposable
 {
 }
 
-public interface IPowerShellInvoker
+public interface IPowerShellInvoker : IPowerShellCliInvoker
 {
 	void SetCurrentWorkingDirectory(string workingDirectory);
 	Task<PowerShellInvocationResult> InvokeExternalScriptAsync(string externalScriptPath, Action<PowerShell>? addParameters = null);
 	Task<PowerShellInvocationResult> InvokeExternalScriptAsync<T>(string externalScriptPath, PSDataCollection<T> input, Action<PowerShell>? addParameters = null);
-
-	Task<PowerShellInvocationResult> InvokeCliAsync(string command, params string[] arguments);
-	Task<PowerShellInvocationResult> InvokeCliAsync<T>(string command, PSDataCollection<T> input, IEnumerable<string> arguments);
 }
 
 public interface IPowerShellCliInvoker

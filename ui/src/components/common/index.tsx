bd23e90a4c9301888fcdce6f82074e01d724d61e
Matt DeKrey <matt.dekrey@principlestudios.com>
2024-03-06 17:21:26 -0600

    Add container components and improve layouts

--- ui/src/components/common/index.tsx
+++ ui/src/components/common/index.tsx
@@ -1,3 +1,4 @@
+export * from './container';
 export * from './list';
 export * from './link';
 export * from './section';
--- ui/src/pages/branch-details/index.tsx
+++ ui/src/pages/branch-details/index.tsx
@@ -1,8 +1,13 @@
 import { useSuspenseQuery } from '@tanstack/react-query';
 import { BranchGraphPresentation } from '../../components/branch-graph/branch-graph.presentation';
+import { Container } from '../../components/common';
 import { queries } from '../../utils/api/queries';
 
 export function BranchDetailsComponent({ name }: { name: string[] }) {
 	const response = useSuspenseQuery(queries.getBranchDetails(name)).data;
-	return <BranchGraphPresentation upstreamData={response} />;
+	return (
+		<Container.Full>
+			<BranchGraphPresentation upstreamData={response} />
+		</Container.Full>
+	);
 }

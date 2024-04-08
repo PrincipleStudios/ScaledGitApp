084e07bd9d537ddf4d951a387861dad0df5fffb5
Matt DeKrey <matt.dekrey@principlestudios.com>
2024-04-08 17:41:30 -0500

    Expose branch type to API

---
+++ Server/BranchingStrategy/BranchingStrategyConfiguration.cs
@@ -0,0 +1,13 @@
+﻿namespace PrincipleStudios.ScaledGitApp.BranchingStrategy;
+
+public class BranchingStrategyConfiguration
+{
+	public required string DefaultBranchType { get; init; }
+	public required Dictionary<string, BranchTypeConfiguration> BranchTypes { get; init; }
+}
+
+public class BranchTypeConfiguration
+{
+	public required string[] Patterns { get; init; }
+	public required string[] Colors { get; init; }
+}

82a40f6554905ef502a31bc8911285f5f26cd8ed
Matt DeKrey <matt.dekrey@principlestudios.com>
2024-04-10 08:13:52 -0500

    Prevent snapping two nodes to the same location

--- ui/src/components/branch-graph/updateScreen.tsx
+++ ui/src/components/branch-graph/updateScreen.tsx
@@ -5,15 +5,24 @@ const gridSize = 10;
 
 export function updateScreen(nodes: BranchGraphNodeDatum[]) {
 	// TODO: track occupied hex locations and prevent multiple in same location
+	const occupied = new Set<string>();
 	for (const d of nodes) {
 		d.x ??= 0;
 		d.y ??= 0;
-		const grid = hexToPixel(pixelToHex({ x: d.x, y: d.y }, gridSize), gridSize);
+		const hex = pixelToHex({ x: d.x, y: d.y }, gridSize);
+		const hexId = `${hex.q},${hex.r}`;
+		if (!occupied.has(hexId)) {
+			occupied.add(hexId);
+			const grid = hexToPixel(hex, gridSize);
 
-		d.screenX += (grid.x - d.screenX) * 0.2;
-		d.screenY += (grid.y - d.screenY) * 0.2;
+			d.screenX += (grid.x - d.screenX) * 0.2;
+			d.screenY += (grid.y - d.screenY) * 0.2;
 
-		d.x += (grid.x - d.x) * 0.05;
-		d.y += (grid.y - d.y) * 0.05;
+			d.x += (grid.x - d.x) * 0.05;
+			d.y += (grid.y - d.y) * 0.05;
+		} else {
+			d.screenX = d.x;
+			d.screenY = d.y;
+		}
 	}
 }

53d06f6158c6f154713c7e33b67329555c6c43e3
Matt DeKrey <matt.dekrey@principlestudios.com>
2024-05-03 08:49:04 -0500

    Extract BranchDetailsMapper

---
+++ Server/Api/Git/Conversions/BranchDetailsMapper.cs
@@ -0,0 +1,38 @@
+﻿using PrincipleStudios.ScaledGitApp.BranchingStrategy;
+using PrincipleStudios.ScaledGitApp.Git.ToolsCommands;
+
+namespace PrincipleStudios.ScaledGitApp.Api.Git.Conversions;
+
+public interface IBranchDetailsMapper
+{
+	BranchDetails ToBranchDetails(UpstreamBranchDetailedState state);
+}
+
+public class BranchDetailsMapper(IBranchTypeLookup branchTypeLookup) : IBranchDetailsMapper
+{
+	public BranchDetails ToBranchDetails(UpstreamBranchDetailedState state)
+	{
+		var type = branchTypeLookup.DetermineBranchType(state.Name);
+
+		return new BranchDetails(
+			state.Name,
+			Type: type.BranchType,
+			Color: type.Color,
+			Exists: state.Exists,
+			NonMergeCommitCount: state.NonMergeCommitCount,
+		Upstream: from upstream in state.Upstreams
+					  let upstreamType = branchTypeLookup.DetermineBranchType(upstream.Name)
+					  select new DetailedUpstreamBranch(
+						  Name: upstream.Name,
+						  Type: upstreamType.BranchType,
+						  Color: upstreamType.Color,
+						  Exists: upstream.Exists,
+						  BehindCount: upstream.BehindCount,
+						  HasConflict: upstream.HasConflict
+					  ),
+			Downstream: from downstream in state.DownstreamNames
+						let downstreamType = branchTypeLookup.DetermineBranchType(downstream)
+						select new Branch(Name: downstream, Color: downstreamType.Color, Type: downstreamType.BranchType)
+		);
+	}
+}

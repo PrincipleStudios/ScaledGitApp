d6a9d20f8e1ff20642f32110beb78e96577fbe3d
Matt DeKrey <matt.dekrey@principlestudios.com>
2024-05-08 11:00:10 -0500

    Add initial modal framework

--- ui/src/components/common/icon-button.tsx
+++ ui/src/components/common/icon-button.tsx
@@ -1,5 +1,6 @@
 import { Button } from './button';
+import { buttonThemes } from './button-themes';
 
 export const IconButton = Button.extend('Button', (T) => (
 	<T className={'p-2 rounded-full text-xl w-auto'} />
-));
+)).themed(buttonThemes);

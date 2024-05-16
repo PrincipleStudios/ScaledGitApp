b5424f2d74fe31ca80d6582b87e6b7d742458191
Matt DeKrey <matt.dekrey@principlestudios.com>
2024-05-16 10:14:53 -0500

    Add implementation for `retrieveGitObject`

---
+++ Server/Git/ToolsCommands/GitGetObject.cs
@@ -0,0 +1,13 @@
+﻿using PrincipleStudios.ScaledGitApp.ShellUtilities;
+
+namespace PrincipleStudios.ScaledGitApp.Git.ToolsCommands;
+
+public class GitGetObject(string objectish) : IPowerShellCommand<Task<string>>
+{
+	public async Task<string> Execute(IPowerShellCommandContext context)
+	{
+		// TODO: What happens if the requested file is binary? Or very large?
+		var cliResults = await context.InvokeCliAsync("git", "cat-file", "-p", objectish);
+		return string.Join('\n', cliResults.ToResultStrings());
+	}
+}
